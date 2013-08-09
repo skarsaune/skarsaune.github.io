c48b4fee19a73178ba54e9e894a905c3ddc9c4b6
Martin Skarsaune <martin.skarsaune@kantega.no>
2013-08-09 20:18:58 +0200

    Norwegian words

---
+++ script/no_NO/words.js
@@ -0,0 +1,17 @@
+/*
+ * Return the complete list of possible words for the language
+ */
+function getAllWords(){
+    return [
+	"og",
+	"el-ler",
+	"men",
+	"Nor-ge",
+	"fa-mi-li-e",
+	"kan-skje",
+	"her",
+	"der",
+	"jeg",
+	"du"];
+	
+} 
