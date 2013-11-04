a31b512a3c4d63617d9e63817b9b5271e93d4143
Martin Skarsaune <martin.skarsaune@kantega.no>
2013-11-04 22:35:28 +0100

    Swipe support

--- script/wordtest.js
+++ script/wordtest.js
@@ -454,6 +454,15 @@ $(document).ready(function() {
     setupToggleButtons();
     sizeWordsToWindow();
     sizeButtonsToWindow();
+    $('.carousel').swipe( {
+	    swipeLeft: function() {
+	        $(this).carousel('next');
+	    },
+	    swipeRight: function() {
+	        $(this).carousel('prev');
+	    },
+	    allowPageScroll: 'vertical'
+	});
 
 });
 // Resize words on window resize
